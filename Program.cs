c67d3ec0de415be63f54283050368033d4e013b5
chienvx <chienvx@ominext.com>
2020-05-12 16:07:45 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace TestConnectRabbitMQ.Sent
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using var channel = connection.CreateModel();
+
+                channel.QueueDeclare(queue: "hello",
+                                        durable: false, //True: Queue vẫn tồn tại nếu nhưng RabitMQ khởi động lại
+                                        exclusive: false, //Được sử dụng bởi chỉ một connection và queue sẽ bị xóa khi connection đó kết thúc
+                                        autoDelete: false, //Queue sẽ bị xóa nếu như consumer cuối cùng hủy subscribe
+                                        arguments: null);
+                Console.WriteLine("Input string to Queue: ");
+                string message = Console.ReadLine();
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine(" [x] Sent {0}", message);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+*	Tutorial Link : https://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
+*	Environment: .Net core 3.1
+*	NugetPacket for two project: RabbitMQ.Client
+*	How to run:
+	1. Start RabbitServer
+	2. Run project "TestConnectRabbitMQ.Receive"  with command: "donet run" 
+	3. Run project "TestConnectRabbitMQ.Sent"  with command: "donet run" & input message
+	4. Message input will show on window console of project "TestConnectRabbitMQ.Receive"
